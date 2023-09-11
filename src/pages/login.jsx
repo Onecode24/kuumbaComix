01152e9957874b77a9750b9673dc3d906f1b560a
Adonis <nobimetanguy19@gmail.com>
2023-09-11 18:17:39 +0100

    Link with Django

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,50 +1,109 @@
+import React, { useState } from 'react';
 import Input from '../components/global/Input'
-import '../styles/index.css' 
-import {Link} from 'react-router-dom'
+import '../styles/index.css'
+import { Link} from 'react-router-dom'
 
 const Login = () => {
-    return (
-        <div className='bg-viewport patternBG flex items-center centerCard pl-10 '>
-            <div className='w-1/2 bg-white py-5 px-10 respCard '>
-                <h2 className='text-3xl montserrat-bold'>Connection</h2>
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
 
-                <form action="" className='my-10'>
-                    {/* <div className='flex justify-between'>
-                        <div className='w-[45%]'>
-                            <Input type='text' label="Nom" name="lastname" />
-                        </div>
-                        <div className='w-[45%]'>
-                            <Input type='text' label="Prénoms" name="firstname" />
-                        </div>
-                    </div> */}
-                    <div className='my-5'>
-                        <Input type='email' label="Adress e-mail" name="email" />
-                    </div>
-                    <div>
-                        <Input type='password' label="Mot de passe" name="password" />
-                    </div>
-                    <div className='flex my-5 justify-between montserrat-regular'>
-                        <div>
-                            <input type="checkbox" name="remenber-me" id="remenber-me" />
-                            <span className='px-2'>Se souvenir de moi</span>
-                        </div>
-                        <p>
-                            <Link >Mot de passe oublié</Link>
-                        </p>
-                    </div>
-                    {/* <div className='my-5'>
-                        <Input type='password' label="Confirmer mot de passe" name="password" />
-                    </div> */}
-                    <input type="button" value="Se connecter" className='montserrat-medium py-3 w-full bg-orange text-lg cursor-pointer' />
-                    <p className='text-gris montserrat-regular py-2 text-center'>Vous n'avez pas un compte?
-                        <span className='text-orange'>
-                            <Link to="/signin"> S'inscrire</Link>
-                        </span>
-                    </p>
-                </form>
+  const [errorMessage, setErrorMessage] = useState('');
+
+ 
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://143.198.201.85/api/token/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        // Extrayez les données JSON de la réponse
+        const data = await response.json();
+    
+        if (data.access) {
+            // Sauvegarde du jeton côté utilisateur
+            localStorage.setItem('tokenkumba', data.access);
+            // Le formulaire a été soumis avec succès, redirigez l'utilisateur
+            alert('Vous êtes maintenant connecté et le token est crée');
+            // Redirigez l'utilisateur vers la page de son choix après la connexion réussie
+            
+          } else {
+            // Gérez les cas où la réponse ne contient pas de jeton
+            console.error('La réponse ne contient pas de jeton.');
+          }
+      } else {
+        // Gérez les erreurs ici, par exemple, affichez un message d'erreur à l'utilisateur
+        setErrorMessage("Email ou mot de passe incorrect.");
+      }
+    } catch (error) {
+      console.error('Erreur lors de la soumission du formulaire', error);
+    }
+  };
+
+  return (
+    <div className='bg-viewport patternBG flex items-center centerCard pl-10 '>
+      <div className='w-1/2 bg-white py-5 px-10 respCard '>
+        <h2 className='text-3xl montserrat-bold'>Connexion</h2>
+
+        <form onSubmit={handleSubmit} className='my-10'>
+          {errorMessage && (
+            <p className='text-red-600 text-sm'>{errorMessage}</p>
+          )}
+          <div className='my-5'>
+            <Input
+              type='text'
+              label="Nom d'utilisateur"
+              name="username"
+              required={true}
+              value={formData.username}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <Input
+              type='password'
+              label="Mot de passe"
+              name="password"
+              required={true}
+              value={formData.password}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className='flex my-5 justify-between montserrat-regular'>
+            <div>
+              <input type="checkbox" name="remember-me" id="remember-me" />
+              <span className='px-2'>Se souvenir de moi</span>
             </div>
-        </div>
-    );
+            <p>
+              <Link to='/forgot-password'>Mot de passe oublié</Link>
+            </p>
+          </div>
+          <input type="submit" value="Se connecter" className='montserrat-medium py-3 w-full bg-orange text-lg cursor-pointer' />
+          <p className='text-gris montserrat-regular py-2 text-center'>Vous n'avez pas de compte?
+            <span className='text-orange'>
+              <Link to="/signin"> S'inscrire</Link>
+            </span>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -1,41 +1,142 @@
-import Input from '../components/global/Input'
-import '../styles/index.css' 
-import {Link} from 'react-router-dom'
+import React, { useState} from 'react';
+import Input from '../components/global/Input';
+import '../styles/index.css';
+import { Link } from 'react-router-dom';
 
 const SignIn = () => {
-    return (
-        <div className='bg-viewport patternBG flex items-center centerCard pl-10 '>
-            <div className='w-1/2 bg-white py-5 px-10 respCard '>
-                <h2 className='text-3xl montserrat-bold'>Créer votre compte</h2>
+  // État local pour stocker les valeurs du formulaire
+  const [formData, setFormData] = useState({
+    last_name: '',
+    first_name: '',
+    email: '',
+    password: '',
+    username: '',
+    confirmPassword: '',
+  });
 
-                <form action="" className='my-10'>
-                    <div className='flex justify-between'>
-                        <div className='w-[45%]'>
-                            <Input type='text' label="Nom" name="lastname" />
-                        </div>
-                        <div className='w-[45%]'>
-                            <Input type='text' label="Prénoms" name="firstname" />
-                        </div>
-                    </div>
-                    <div className='my-5'>
-                        <Input type='email' label="Adress e-mail" name="email" />
-                    </div>
-                    <div>
-                        <Input type='password' label="Mot de passe" name="password" />
-                    </div>
-                    <div className='my-5'>
-                        <Input type='password' label="Confirmer mot de passe" name="password" />
-                    </div>
-                    <input type="button" value="S'inscrire" className='montserrat-medium py-3 w-full bg-orange text-lg cursor-pointer' />
-                    <p className='text-gris montserrat-regular py-2 text-center'>Vous avez déjà un compte?
-                        <span className='text-orange'>
-                            <Link to="/login"> Se connecter</Link>
-                        </span>
-                    </p>
-                </form>
+  const [errorMessage, setErrorMessage] = useState('');
+
+  // Fonction de gestion des changements de saisie dans les champs du formulaire
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  // Fonction de gestion de la soumission du formulaire
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Vérifiez si les deux mots de passe sont identiques
+    if (formData.password.trim() !== formData.confirmPassword.trim()) {
+      setErrorMessage("Les mots de passe ne correspondent pas.");
+      return; // Arrêtez la soumission du formulaire si les mots de passe ne correspondent pas
+    }
+
+    // Définissez le champ 'username' avec la valeur de 'email'
+    formData.username = formData.email;
+
+    try {
+      // Utilisez la fonction fetch pour envoyer les données du formulaire à votre endpoint
+      const response = await fetch('http://143.198.201.85/api/user/', {
+        method: 'POST', // ou 'PUT' ou 'PATCH' en fonction de votre API
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        // Le formulaire a été soumis avec succès, redirigez ou effectuez d'autres actions ici
+       alert('Formulaire soumis avec succes');
+      } else {
+        // Gérez les erreurs ici, par exemple, affichez un message d'erreur à l'utilisateur
+        setErrorMessage('Cet utilisateur existe déjà !');
+      }
+    } catch (error) {
+      console.error('Erreur lors de la soumission du formulaire', error);
+    }
+  };
+
+  return (
+    <div className='bg-viewport patternBG flex items-center centerCard pl-10 '>
+      <div className='w-1/2 bg-white py-5 px-10 respCard '>
+        <h2 className='text-3xl montserrat-bold'>Créer votre compte</h2>
+
+        <form onSubmit={handleSubmit} className='my-10'>
+          <div className='flex justify-between'>
+            <div className='w-[45%]'>
+              <Input
+                type='text'
+                label='Nom'
+                name='last_name'
+                required={true}
+                value={formData.last_name}
+                onChange={handleInputChange}
+              />
             </div>
-        </div>
-    );
+            <div className='w-[45%]'>
+              <Input
+                type='text'
+                label='Prénoms'
+                name='first_name'
+                required={true}
+                value={formData.first_name}
+                onChange={handleInputChange}
+              />
+            </div>
+          </div>
+          <div className='my-5'>
+            <Input
+              type='email'
+              label='Adresse e-mail'
+              name='email'
+              required={true}
+              value={formData.email}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <Input
+              type='password'
+              label='Mot de passe'
+              name='password'
+              required={true}
+              value={formData.password}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className='my-5'>
+            <Input
+              type='password'
+              label='Confirmer mot de passe'
+              name='confirmPassword'
+              required={true}
+              value={formData.confirmPassword}
+              onChange={handleInputChange}
+            />
+          </div>
+          {errorMessage && (
+            <p className='text-red-600 text-sm'>{errorMessage}</p>
+          )}
+          <button
+            type='submit'
+            className='montserrat-medium py-3 w-full bg-orange text-lg cursor-pointer'
+          >
+            S'inscrire
+          </button>
+          <p className='text-gris montserrat-regular py-2 text-center'>
+            Vous avez déjà un compte?
+            <span className='text-orange'>
+              <Link to='/login'> Se connecter</Link>
+            </span>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
